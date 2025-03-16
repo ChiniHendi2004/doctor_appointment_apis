a6691e6455c7c28a1e255098597531a2869b905b
Chini Hendi <chinihendi88@gmail.com>
2025-03-17 01:48:10 +0530

    Added cache clear route

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -18,9 +19,11 @@ Route::middleware([
 });
 
 // Route to clear cache
+
 Route::get('/clear-cache', function () {
     Artisan::call('config:clear');
     Artisan::call('cache:clear');
     Artisan::call('config:cache');
     return response()->json(['message' => 'Cache cleared successfully!']);
 });
+
